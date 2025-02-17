5ebed999d04a0228178ce3c99f20ef0a78080f12
Dabeer Shaikh <dab8106@gmail.com>
2025-02-17 05:58:50 +0000

    adding recaptcha feature

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,20 @@
+# filepath: todoapp/app/__init__.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
 
 db = SQLAlchemy()
+csrf = CSRFProtect()
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
     app.config['SECRET_KEY'] = 'your_secret_key'
+    app.config['RECAPTCHA_PUBLIC_KEY'] = 'your_public_key'
+    app.config['RECAPTCHA_PRIVATE_KEY'] = 'your_private_key'
 
     db.init_app(app)
+    csrf.init_app(app)
 
     with app.app_context():
         from . import views
--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,5 @@
-from flask_wtf import FlaskForm
+# filepath: todoapp/app/forms.py
+from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, TextAreaField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
@@ -6,4 +7,5 @@ class TaskForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     description = TextAreaField('Description')
     done = BooleanField('Done')
+    recaptcha = RecaptchaField()
     submit = SubmitField('Add Task')
